11aa15c6716568017cda3b40cb9a1d05ce50bb33
JustBrad69 <bradisjust@gmail.com>
2025-06-07 10:06:17 +0300

    Remove Voiceflow Chatbot Widget

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import HelpSupport from './components/HelpSupport';
 import Footer from './components/Footer';
 import WhatsAppWidget from './components/WhatsAppWidget';
 import WifiNotification from './components/WifiNotification';
-import VoiceflowWidget from './components/VoiceflowWidget';
+import TrafficChecker from './components/TrafficChecker';
 
 function App() {
   return (
@@ -35,7 +35,7 @@ function App() {
         <HelpSupport />
       </main>
       <Footer />
-      <VoiceflowWidget />
+      <TrafficChecker />
       <WhatsAppWidget />
     </div>
   );
