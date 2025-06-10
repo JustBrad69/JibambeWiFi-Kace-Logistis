85cc6cfdc1727f3030657345ce84c5782cc3f203
JustBrad69 <bradisjust@gmail.com>
2025-06-10 10:17:16 +0300

    Remove TIXAE Chatbot Component

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import HelpSupport from './components/HelpSupport';
 import Footer from './components/Footer';
 import WhatsAppWidget from './components/WhatsAppWidget';
 import WifiNotification from './components/WifiNotification';
-import TixaeChatbot from './components/TixaeChatbot';
 
 function App() {
   return (
@@ -36,7 +35,6 @@ function App() {
       </main>
       <Footer />
       <WhatsAppWidget />
-      <TixaeChatbot />
     </div>
   );
 }
